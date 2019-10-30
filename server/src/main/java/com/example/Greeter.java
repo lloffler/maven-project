5dfaf2749b5af6ccd8d517076f0d5d391bc6c715
lloffler <49196029+lloffler@users.noreply.github.com>
2019-10-30 10:19:59 +0100

    javadoc

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -3,11 +3,15 @@ package com.example;
 
 public class Greeter {
 
-  
+  /**
+  * Constructeur javadoc test jenkins
+  */
   public Greeter() {
 
   }
-
+/**
+* Méthode javadoc test jenkins
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
